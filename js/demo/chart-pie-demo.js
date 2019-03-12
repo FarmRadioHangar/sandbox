92afc60c653109380fe937214c4824131ea3187e
Johannes Hildén <hildenjohannes@gmail.com>
2019-03-12 15:27:22 +0300

    disable tooltip

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -18,18 +18,19 @@ var myPieChart = new Chart(ctx, {
   options: {
     maintainAspectRatio: false,
     tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
+      enabled: false
+      //backgroundColor: "rgb(255,255,255)",
+      //bodyFontColor: "#858796",
+      //borderColor: '#dddfeb',
+      //borderWidth: 1,
+      //xPadding: 15,
+      //yPadding: 15,
+      //displayColors: false,
+      //caretPadding: 10,
     },
     legend: {
       display: false
     },
-    cutoutPercentage: 80,
+    cutoutPercentage: 70,
   },
 });
